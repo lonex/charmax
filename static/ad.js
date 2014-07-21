fc5cce662d8fc3b171f431f41576cf373952268f
lonex <stonelonely@gmail.com>
2014-07-21 15:44:58 -0700

    lay out the foundation of the algorithm; generate output for debug.

--- static/ad.js
+++ static/ad.js
@@ -1,4 +1,55 @@
+/**
+ * [Plugin style function]
+ * @param  {[type]} $ [description]
+ * @return {[type]}   [description]
+ */
+(function($) {
+  $.fn.maxChar = function() {
+
+    var shadowIdSuffix = "_shadow";
+
+    function createShadow(pEl) {
+      var shadow = 
+        $(pEl.cloneNode(true))
+         .attr('id', $(pEl).attr('id') + shadowIdSuffix)
+         .hide()
+         .css({
+            'position': 'absolute',
+            'overflow': 'visible', 
+            'left': '800px'
+         })
+         .width($(pEl).width())
+         .height('auto');
+      $(pEl).after(shadow);
+      return shadow;
+    };
+
+    function compare(shadow, original) {
+      return shadow.height() > original.height();
+    };
+
+    return this.each(function() {
+      var me = $(this);
+
+      var shadow = createShadow(this);
+      var content = me.html();
+
+      while (content.length > 0 && compare(shadow, me)) {
+        content = content.slice(0, -1);
+        shadow.html(content);
+      }
+
+      me.html(content);
+    });
+  };
+
+})(jQuery);
+
+
+/**
+ * [description]
+ * @return {[type]} [description]
+ */
 $(function() {
-  $("#done").text("loaded and can be manipulated." );
-  $("#done").css("display", "block");
+  $('.measure').maxChar();
 });
