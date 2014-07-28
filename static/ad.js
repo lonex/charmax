3447416cd55766c8e323729d4754047938c134cd
lonex <stonelonely@gmail.com>
2014-07-28 13:06:35 -0700

    Remove google font url as it's not necessary; add more chars in sample text

--- static/ad.js
+++ static/ad.js
@@ -12,8 +12,10 @@
                 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
                 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor',
                 'in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,',
-                'sunt in culpa qui officia deserunt mollit anim id est laborum'
-
+                'sunt in culpa qui officia deserunt mollit anim id est laborum',
+                'Donec non sollicitudin metus. Integer mi velit, auctor lacinia nibh at, fringilla venenatis metus. Mauris egestas tortor arcu.',
+                'Praesent laoreet arcu in hendrerit dapibus. Curabitur augue nisl, sodales pharetra tortor in, semper venenatis nisl. Nunc sit amet',
+                'rhoncus mauris. Etiam eros arcu, venenatis sed viverra quis, bibendum non nunc. Praesent eu rhoncus libero.'
             ].join(' ')
         };
 
